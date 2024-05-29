1b9df984ccbced94eecc216faed5f1045036854a
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2024-05-29 11:31:11 +0200

    palette couleur

---
+++ TP/TP_Image/src/Palette.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+public class Palette {
+   Color[] colors;
+
+   public Palette(Color[] c){
+       colors = c;
+   }
+
+   public Color getPlusProche(Color colorCompared){
+       double valPlusProche = Double.MAX_VALUE;
+       Color colorPlusProche = null;
+
+       for(Color c  : colors){
+           double distance = ImageCopy.distanceBetweenColor(c,colorCompared);
+           if(distance <valPlusProche){
+               valPlusProche = distance;
+               colorPlusProche = c;
+           }
+       }
+       return colorPlusProche;
+   }
+
+}
