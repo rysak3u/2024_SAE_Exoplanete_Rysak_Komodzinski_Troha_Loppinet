aaab6220e56ebf3d86b15eb983be8b8167e13ed3
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2024-06-18 14:25:17 +0200

    CieLab

--- TP/TP_Image/src/Palette.java
+++ TP/TP_Image/src/Palette.java
@@ -2,16 +2,18 @@ import java.awt.*;
 
 public class Palette {
    Color[] colors;
+   NormeCouleurs distanceCouleur;
 
-   public Palette(Color[] c){
+   public Palette(Color[] c, NormeCouleurs dC){
        colors = c;
+       distanceCouleur = dC;
    }
 
    public Color getPlusProche(Color colorCompared){
        double valPlusProche = Double.MAX_VALUE;
        Color colorPlusProche = null;
        for(Color c  : colors){
-           double distance = ImageCopy.distanceBetweenColor(c,colorCompared);
+           double distance = distanceCouleur.distanceCouleur(c,colorCompared);
            if(distance <valPlusProche){
                valPlusProche = distance;
                colorPlusProche = c;
